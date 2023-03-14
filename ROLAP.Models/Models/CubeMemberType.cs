28241d19124a7fddf38d67957ec6a936f35d0580
NikolayS <slezenkonikolay@mail.ru>
2023-03-14 20:34:49 +0700

    Модель CubeRequest

---
+++ ROLAP.Models/Models/CubeMemberType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROLAP.Model.Models
+{
+    public enum CubeMemberType
+    {
+        Dimension = 0,
+        Measure
+    }
+}
