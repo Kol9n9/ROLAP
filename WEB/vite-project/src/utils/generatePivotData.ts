6d82e268f308dec595f4ae8419037e66597acb60
Slezenko Nikolay <slezenko@incom.tomsk.ru>
2025-04-22 19:53:51 +0700

    Исправление ошибки

--- WEB/vite-project/src/utils/generatePivotData.ts
+++ WEB/vite-project/src/utils/generatePivotData.ts
@@ -166,7 +166,7 @@ function getColumns(columns: HeaderCellModel): HeaderTr[] {
             for (const item of currentItemDepthLevels[hierarchy]) {
                 const childHierarchy = item.item.Hierarchy.split('.').slice(0, -1).join('.');
                 if (item.item.Children.length && !item.item.Children[0].Hierarchy.startsWith(childHierarchy)) item.td.RowSpan = maxDepthLevel;
-                if(!item.item.Children.length && item.item.Parent?.Hierarchy === item.item.Hierarchy) item.td.RowSpan = maxDepthLevel;
+                if (!item.item.Children.length) item.td.RowSpan = maxDepthLevel + 1;
             }
         }
     }
